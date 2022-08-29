0834ef163825f7102e721c3f1e88e80d148da11a
Isaac Pérez <56978632+Isaac13Di00@users.noreply.github.com>
2022-08-29 12:25:31 -0600

    Base movimiento/coliciones

---
+++ script.js
@@ -0,0 +1,241 @@
+var cv, ctx, blackground="white", speed=40, player_Direction="", bomb_Direction="right";
+var pause=false;
+var bomb_flag = false;
+//var pl = new Image();
+//Other functions
+function clear(){
+    ctx.fillStyle = blackground;
+    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+}
+function random(){
+    return  (Math.round(Math.random()) == 1)?true:false;
+}
+//Principal functions
+function run(){
+    cv = document.getElementById('mycanvas');
+    ctx = cv.getContext('2d');
+    //pl.src = '/img/player.png';
+    player1 = new Player(0, 0, 40, 40, "red");
+    bomb = new Bomb(-40, -40, false);
+    //wall = new Block(40,40,40,40,false);
+    walls = [];
+    blocks = [];
+    walls.unshift(new Wall(40,40,40,40));
+    blocks.unshift(new Block(80,0,40,40,true));
+    //blocks.unshift(new Block(0,80,40,40,true));
+    //Generate walls
+    for (let index = 0; index < 8; index++) {
+        for (let index = 0; index < 10; index++) {
+            if (walls[0].x <= 640) {
+                walls.unshift(new Wall(walls[0].x+80, walls[0].y, 40, 40));
+            }else{
+                walls.unshift(new Wall(40, walls[0].y+80, 40, 40));
+            }
+        }
+    }
+    //Generate blocks
+    for (let index = 0; index < 24; index++) {
+        for (let index = 0; index < 24; index++) {
+            if (blocks[0].x <= 680) {
+                blocks.unshift(new Block(blocks[0].x+40, blocks[0].y, 40, 40, random()));
+            }else if(blocks[0].y == 0 && blocks[0].x > 680 ){
+                blocks.unshift(new Block(80, blocks[0].y+40, 40, 40, random()));
+            }else{
+                blocks.unshift(new Block(0, blocks[0].y+40, 40, 40, random()));
+            }
+        }
+    }
+    paint();
+}
+function paint(){
+    window.requestAnimationFrame(paint);
+    clear()
+    //ctx.drawImage(pl, player1.x, player1.y);
+    //ctx.drawImage(aceite, player2.x, player2.y);
+    for (let index = 0; index < blocks.length; index++) {
+        blocks[index].paint(ctx);
+    }
+    for (let index = 0; index < walls.length; index++) {
+        walls[index].paint(ctx);
+    }
+    player1.paint(ctx);
+    bomb.paint(ctx, bomb_Direction, player1);
+    /*for (let index = 0; index < pared.length; index++) {
+        ctx.drawImage(poste, pared[index].x, pared[index].y);
+    }
+    ctx.fillStyle = "black";
+    ctx.fillText("SCORE: "+score,440,10);*/
+    if(pause){
+        ctx.fillStyle = "rgba(0,0,0,0.5)";
+        ctx.fillRect(0,0,760,760);
+        ctx.fillStyle = "white";
+        ctx.fillText("Pause",245,250);
+    }else{
+        update();
+    }
+}
+function update(){ 
+    //Validation move
+    if(player1.x > 720){player1.x = 720;}
+    if(player1.x < -1){player1.x = 0;}
+    if(player1.y > 720){player1.y = 720;}
+    if(player1.y < 0){player1.y = 0;}
+    //Move player
+    if(player_Direction=="right"){player1.x+=speed;bomb_flag=true;}
+    if(player_Direction=="left"){player1.x-=speed;bomb_flag=true;}
+    if(player_Direction=="down"){player1.y+=speed;bomb_flag=true;}
+    if(player_Direction=="up"){player1.y-=speed;bomb_flag=true;}
+    //Validation collision
+    for (let index = 0; index < walls.length; index++) {
+        if(player1.collision(walls[index])){
+            if(player_Direction=="right"){player1.x-=speed;bomb_flag=false;}
+            if(player_Direction=="left"){player1.x+=speed;bomb_flag=false;}
+            if(player_Direction=="down"){player1.y-=speed;bomb_flag=false;}
+            if(player_Direction=="up"){player1.y+=speed;bomb_flag=false;}
+        }
+    }
+    for (let index = 0; index < blocks.length; index++) {
+        if(player1.collision(blocks[index]) && blocks[index].v == true){
+            if(player_Direction=="right"){player1.x-=speed;bomb_flag=false;}
+            if(player_Direction=="left"){player1.x+=speed;bomb_flag=false;}
+            if(player_Direction=="down"){player1.y-=speed;bomb_flag=false;}
+            if(player_Direction=="up"){player1.y+=speed;bomb_flag=false;}
+        }
+    }
+    player_Direction="";
+}
+document.addEventListener('keydown', (e)=>{
+    //left
+    if (e.keyCode == 65 || e.keyCode == 37) {
+        player_Direction = "left";bomb_Direction = "left";
+    }
+    //down
+    if (e.keyCode == 83 || e.keyCode == 40) {
+        player_Direction = "down";bomb_Direction = "down";
+    }
+    //right
+    if (e.keyCode == 68 || e.keyCode == 39) {
+        player_Direction = "right";bomb_Direction = "right";
+    }
+    //up
+    if (e.keyCode == 87 || e.keyCode == 38) {
+        player_Direction = "up";bomb_Direction = "up";
+    }
+    //space bar
+    if(e.keyCode == 32){
+        if(bomb_flag && !bomb.v){
+            bomb.create_bomb(ctx, bomb_Direction, player1.x, player1.y);
+        }
+        setTimeout(function(){
+            bomb.v=false;
+            bomb.delete_bomb(ctx);
+            for (let index = 0; index < blocks.length; index++) {
+                if(bomb.collision(blocks[index]) && blocks[index].v == true){
+                   console.log("a");
+                }
+            }
+        }, 2000);
+    }
+    //pause
+    if(e.keyCode == 13){
+        pause = (!pause)?true:false;
+    }
+});
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 17);
+    };
+}());
+window.addEventListener("load", run, false);
+function Block(x, y, w, h, v){
+    this.x = x;this.y = y;
+    this.w = w;this.h = h;
+    this.c = "brown";
+    this.v = v;
+    this.paint = function paint(ctx){
+        if(this.v){
+            ctx.strokeRect(this.x, this.y, this.w, this.h);
+            ctx.fillStyle=this.c;
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+        }
+    }
+}
+function Wall(x, y, w, h){
+    this.x = x;this.y = y;
+    this.w = w;this.h = h;
+    this.c = "gray";
+    this.paint = function paint(ctx){
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle=this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+}
+function Bomb(x, y, v){
+    this.x = x;this.y = y;
+    this.w = 40;this.h = 40;
+    this.c = "black";
+    this.v = false;
+    this.paint = function(ctx){
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle=this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    };
+    this.create_bomb = function(ctx, direction, x, y){
+        if (direction=="up") {
+            this.x = x;
+            this.y = y-40;
+        }else if(direction=="right"){
+            this.x = x+40;
+            this.y = y;
+        }else if(direction=="left"){
+            this.x = x-40;
+            this.y = y;
+        }else if(direction=="down"){
+            this.x = x;
+            this.y = y+40;
+        }
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle=this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    };
+    this.delete_bomb = function(ctx){
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle=this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        this.x = -80;
+        this.y = -80;
+    };
+    this.collision = function (target) { 
+        if(this.x < target.x + target.w){
+            
+        }if(this.x + this.w > target.x){
+
+        }if(this.y < target.y + target.h){
+
+        }if(this.y + this.h > target.y){
+            
+        }
+        return true;
+    };
+}
+function Player(x, y, w, h, c){
+    this.x = x;this.y = y;
+    this.w = w;this.h = h;
+    this.c = c;
+    this.paint = function(ctx){
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+        ctx.fillStyle=this.c;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    };
+    this.collision = function (target) { 
+        if(this.x < target.x + target.w &&
+            this.x + this.w > target.x && 
+            this.y < target.y + target.h && 
+            this.y + this.h > target.y){
+                return true;
+        }
+    }
+}
